4940f377299d3c984236eca45e5ff6b0306a5031
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2024-10-15 21:51:12 +0900

    Add files via upload

---
+++ 1. java/source/day01/src/Step01.java
@@ -0,0 +1,5 @@
+public class Step01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
---
+++ 1. java/source/day01/src/exam01/Ex01.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int num; // 변수 선언 => 공간 할당(값을 다룰 수 있는 공간 할당 => 메모리 할당)
+        num = 10; // 대입 연산자(=): 오른쪽 값을 왼쪽 변수에 저장하는 것
+
+        int num2 = 20; // 선언과 동시에 초기화(변수에 최초로 값을 저장한 것)
+
+//        System.out.println(num2);
+
+    }
+}
